b1afaf84c02514224d1e4560843af8c2268cb638
xnubber <aabbccdd10902@gmail.com>
2022-03-04 22:37:09 +0800

    feat: add ejs-mate & boilerplate.ejs

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
 const port = 3000
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 const db = mongoose.connection
@@ -14,13 +16,14 @@ db.once('open', () => {
 
 const app = express()
 
-app.set('view engine', ejs)
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_mehtod'))
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
   res.render('home')
 })
 

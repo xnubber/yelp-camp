7c7058ea3e9dcd6a56d1b7a3d795db88dfac7f3b
xnubber <aabbccdd10902@gmail.com>
2022-03-15 01:22:35 +0800

    feat: add catch async & express error

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
 const port = 3000
 
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 const db = mongoose.connection
@@ -28,46 +31,51 @@ app.get('/', (req, res) => {
 })
 
 // all campgrounds
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
-})
+}))
 
 // create a new campground
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res) => {
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // read a campground page
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/show', { campground })
-})
+}))
 
 // update a campground
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/edit', { campground })
-})
+}))
 
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', catchAsync(async(req, res) => {
   const {id} = req.params
   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // delete a campground
-app.delete('/campgrounds/:id', async(req, res)=> {
+app.delete('/campgrounds/:id', catchAsync(async(req, res)=> {
   const {id} = req.params
   await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds')
-})
+}))
+
+app.use((err, req, res, next) => {
+  res.send('something went wrong')
+}) 
+
 
 app.listen(port, ()=> {
   console.log(`Serving on port ${port}`)

47cb79d33228526f39ee4e7b35a9f6a0aca90851
xnubber <aabbccdd10902@gmail.com>
2022-03-16 15:15:41 +0800

    feat: add Joi package to help back-end validation

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = 3000
 
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
+const Joi = require('joi')
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -42,7 +43,20 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res) => {
-  if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 404)
+  const campgroundSchema = Joi.object({
+    campground: Joi.object({
+      title: Joi.string().required(),
+      price: Joi.number().required().min(0),
+      image: Joi.string().required(),
+      location: Joi.string().required(),
+      description: Joi.string().required()
+    }).required()
+  })
+  const {error} = campgroundSchema.validate(req.body)
+  if(error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  }
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`/campgrounds/${campground._id}`)

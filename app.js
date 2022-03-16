84e6e9b2de73c2c110c04900a7ba19ba2247d4c2
xnubber <aabbccdd10902@gmail.com>
2022-03-16 14:46:28 +0800

    feat: add error page

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res) => {
+  if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 404)
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`/campgrounds/${campground._id}`)
@@ -72,8 +73,13 @@ app.delete('/campgrounds/:id', catchAsync(async(req, res)=> {
   res.redirect('/campgrounds')
 }))
 
+app.all('*', (req,res, next) => {
+  next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err, req, res, next) => {
-  res.send('something went wrong')
+  const {statusCode = 500, message = 'Something went wrong'} = err
+  res.status(statusCode).render('campgrounds/error', {err})
 }) 
 
 
